94dabc3624fa4adb0060e17bac78732827c33361
it512 <it512@msn.com>
2024-05-24 16:32:58 +0800

    inst

---
+++ mongo/inst.go
@@ -0,0 +1,25 @@
+package mongo
+
+import (
+	"context"
+
+	"github.com/it512/loong"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func (m *Store) LoadProcInst(ctx context.Context, instID string, p *loong.ProcInst) error {
+	sr := m.instC.FindOne(ctx, bson.D{{Key: "inst_id", Value: instID}})
+	if sr.Err() != nil {
+		return sr.Err()
+	}
+	return sr.Decode(p)
+}
+
+func (m *Store) CreateProcInst(ctx context.Context, procInst *loong.ProcInst) error {
+	return nil
+}
+
+func (m *Store) EndProcInst(ctx context.Context, procInst *loong.ProcInst) error {
+	_, err := m.instC.UpdateOne(ctx, bson.D{{Key: "inst_id", Value: procInst.InstID}}, nil)
+	return err
+}
--- mongo/mongo.go
+++ mongo/mongo.go
@@ -1,1 +1,10 @@
 package mongo
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type Store struct {
+	db    *mongo.Database
+	instC *mongo.Collection
+	taskC *mongo.Collection
+	execC *mongo.Collection
+}
