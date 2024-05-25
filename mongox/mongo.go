e62c45c68251619b2ad381e0eb3023e98afaeeb5
it512 <it512@msn.com>
2024-05-26 01:03:19 +0800

    rename

--- mongox/mongo.go
+++ mongox/mongo.go
@@ -1,8 +1,10 @@
-package mongo
+package mongox
 
 import "go.mongodb.org/mongo-driver/mongo"
 
 type Store struct {
+	client *mongo.Client
+
 	db    *mongo.Database
 	instC *mongo.Collection
 	taskC *mongo.Collection
