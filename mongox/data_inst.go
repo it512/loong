3ad29a91a414952fb1f2f5ff1c1666e9e7540a9f
it512 <it512@msn.com>
2024-06-15 23:28:45 +0800

    add inst

---
+++ mongox/data_inst.go
@@ -0,0 +1,26 @@
+package mongox
+
+import (
+	"time"
+
+	"github.com/it512/loong"
+)
+
+type inst struct {
+	InstID string `json:"inst_id,omitempty"`
+	ProcID string `json:"proc_id,omitempty"`
+
+	Starter string `json:"starter,omitempty"`
+
+	BusiKey  string `json:"busi_key,omitempty"`
+	BusiType string `json:"busi_type,omitempty"`
+
+	StartTime time.Time `json:"start_time,omitempty"`
+	EndTime   time.Time `json:"end_time,omitempty"`
+
+	Status int `json:"status,omitempty"`
+
+	Init loong.Var `json:"init,omitempty"`
+
+	Tags loong.Var `json:"tags,omitempty"`
+}
