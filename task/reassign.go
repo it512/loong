dc87353609294b1e62895d9aaaf2ebd090957b60
it512 <it512@msn.com>
2024-06-29 15:17:16 +0800

    renew

--- task/reassign.go
+++ task/reassign.go
@@ -2,6 +2,7 @@ package task
 
 import (
 	"context"
+	"time"
 
 	"github.com/it512/loong"
 )
@@ -10,17 +11,36 @@ type ReassignCmd struct {
 	TaskID string
 
 	*loong.Engine
+
+	ReassingFn func(context.Context, *loong.UserTask) error
 }
 
-func (r *ReassignCmd) Init(ctx context.Context, e *loong.Engine) error {
+func (r *ReassignCmd) Bind(ctx context.Context, e *loong.Engine) error {
 	r.Engine = e
 	return nil
 }
 
 func (r ReassignCmd) Do(ctx context.Context) error {
-	u := &loong.UserTask{}
-	if err := r.Engine.Storer.LoadUserTask(ctx, r.TaskID, u); err != nil {
+	u := loong.UserTask{}
+	if err := r.Engine.Storer.LoadUserTask(ctx, r.TaskID, &u); err != nil {
 		return err
 	}
-	return nil
+
+	ucopy := u // copy
+
+	if err := r.ReassingFn(ctx, &ucopy); err != nil {
+		return err
+	}
+
+	if err := r.Storer.EndUserTask(ctx, u); err != nil {
+		return err
+	}
+
+	u.TaskID = r.Engine.NewID()
+	u.Assignee = ucopy.Assignee
+	u.CandidateGroups = ucopy.CandidateGroups
+	u.CandidateUsers = ucopy.CandidateUsers
+	u.StartTime = time.Now()
+
+	return r.Engine.Storer.CreateTasks(ctx, u)
 }
