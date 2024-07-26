811d67bac946faf910fecb94e86ac2d2b5260fc1
it512 <it512@msn.com>
2024-07-26 22:11:29 +0800

    add func

--- bpmn/boundar_event.go
+++ bpmn/boundar_event.go
@@ -4,6 +4,7 @@ import "github.com/it512/loong/bpmn/zeebe"
 
 type TBoundaryEvent struct {
 	Id             string `xml:"id,attr"`
+	Name           string `xml:"name,attr"`
 	CancelActivity bool   `xml:"cancelActivity,attr"`
 	AttachedToRef  string `xml:"attachedToRef,attr"`
 
@@ -13,4 +14,16 @@ type TBoundaryEvent struct {
 	TimerEventDefinition   []TTimerEventDefinition   `xml:"timerEventDefinition"`
 
 	Output []zeebe.TIoMapping `xml:"extensionElements>ioMapping>output"`
+}
+
+func (e TBoundaryEvent) GetId() string {
+	return e.Id
+}
+
+func (e TBoundaryEvent) GetName() string {
+	return e.Name
+}
+
+func (e TBoundaryEvent) GetOutgoingAssociation() []string {
+	return e.OutgoingAssociation
 }
