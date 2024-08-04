a8e208418476fa7f6d61b946ad8e1b9eb05dd043
it512 <it512@msn.com>
2024-08-05 00:13:54 +0800

    fix test

--- var_test.go
+++ var_test.go
@@ -7,8 +7,8 @@ import (
 func TestVarChange(t *testing.T) {
 	var v Variable
 	v.Exec.ProcInst = &ProcInst{Var: NewVar()}
-	v.Put("x", "x")
-	if !v.Changed() {
+	v.Put("Var.x", "x")
+	if !v.isVarChanged {
 		t.Errorf("not changed")
 	}
 }
